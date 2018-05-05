ddf3e354616634b497c334e6d34e2452e4883c99
zhangxianhua01 <zhangxianhua01@baidu.com>
2018-05-05 16:47:55 +0800

    增加RxAndroid进行线程调度，实现io线程主线程之间的切换

--- app/src/main/java/com/hteng/okhttp/activity/MainActivity.java
+++ app/src/main/java/com/hteng/okhttp/activity/MainActivity.java
@@ -3,16 +3,27 @@ package com.hteng.okhttp.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 import com.hteng.okhttp.R;
 import com.hteng.okhttp.util.NetWorkUtil;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String REQUEST_URL = "https://www.baidu.com";
 
-    private void initView() {
+    private TextView mTextView;
 
+    private void initView() {
+        mTextView = findViewById(R.id.get_req_text);
     }
 
     private void initData() {
@@ -33,12 +44,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onGetClick(View view) {
-        new Thread() {
+        Observable.create(new ObservableOnSubscribe<String>() {
+
             @Override
-            public void run() {
-                super.run();
-                NetWorkUtil.get(REQUEST_URL);
+            public void subscribe(ObservableEmitter<String> emitter) throws Exception {
+                String result = NetWorkUtil.get(REQUEST_URL);
+                emitter.onNext(result);
             }
-        }.start();
+        }).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        mTextView.setVisibility(View.VISIBLE);
+                        mTextView.setText(s);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
     }
 }
